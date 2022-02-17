1f73f807f38254767fbdd59f146268b03e65ffb1
Alexander <alez.grante@gmail.com>
2022-02-17 23:19:42 +0300

    QrConeApp.kt and manifest name fixed

--- app/src/main/java/com/example/qrcone/core/QrConeApp.kt
+++ app/src/main/java/com/example/qrcone/core/QrConeApp.kt
@@ -2,7 +2,7 @@ package com.example.qrcone.core
 
 import android.app.Application
 
-class QrCodeApp: Application() {
+class QrConeApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
