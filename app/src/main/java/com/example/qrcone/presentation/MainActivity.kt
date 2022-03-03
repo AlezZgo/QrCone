56c073f8be2a6063d229a42dc5dee50149f038b5
Alexander <alez.grante@gmail.com>
2022-03-03 10:20:04 +0300

    QC01_AddQr colored

--- app/src/main/java/com/example/qrcone/presentation/MainActivity.kt
+++ app/src/main/java/com/example/qrcone/presentation/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
     private val component by lazy {
         (application as QrConeApp).component
     }
